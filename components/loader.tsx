ad52c2a152d804d5ebf55a87e313cad6a82cf433
Felipe Saff <luizsaff@tutanota.com>
2025-09-01 14:30:23 -0400

    feat: add loader component

---
+++ components/loader.tsx
@@ -0,0 +1,15 @@
+export function Loader() {
+	return (
+		<div className="flex items-center justify-center min-h-screen">
+			<div className="relative">
+				<div className="relative w-32 h-32">
+					<div className="absolute w-full h-full rounded-full border-[3px] duration-[3s] border-gray-100/10 border-r-[#0ff] border-b-[#0ff] animate-spin"></div>
+
+					<div className="absolute w-full h-full duration-[2s] direction-reverse rounded-full border-[3px] border-gray-100/10 border-t-[#0ff] animate-spin"></div>
+				</div>
+
+				<div className="absolute inset-0 bg-gradient-to-tr from-[#0ff]/10 via-transparent to-[#0ff]/5 animate-pulse rounded-full blur-sm"></div>
+			</div>
+		</div>
+	);
+}
